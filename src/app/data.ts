8db1ae33bce261f5f26c0a604e414fd6e10116e8
Sandeep <sandeepdewangan7@gmail.com>
2025-01-30 20:48:49 +0530

    Property Binding, Dynamic Value, Assets

---
+++ src/app/data.ts
@@ -0,0 +1,27 @@
+export const users = [
+    {
+        id: '101',
+        name: 'Sandeep',
+        avatar: 'user.png',
+    },
+    {
+        id: '102',
+        name: 'Khushbu',
+        avatar: 'user.png',
+    },
+    {
+        id: '103',
+        name: 'Cherry',
+        avatar: 'user.png',
+    },
+    {
+        id: '104',
+        name: 'Unnati',
+        avatar: 'user.png',
+    },
+    {
+        id: '105',
+        name: 'Garv',
+        avatar: 'user.png',
+    }
+];
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { users } from '../data';
+
+const randomNum = Math.floor(Math.random() * users.length);
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = users[randomNum];
+}
